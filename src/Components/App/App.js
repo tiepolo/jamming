f46133dd69c9f9e9f61e4f1c43b1f3382d465bb8
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-10-18 15:58:57 -0700

    Added several files.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
       </div>
     );
   }
