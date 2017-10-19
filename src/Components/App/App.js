b899143fafdc98bc8605a833e2294951efd0b048
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-10-19 12:40:34 -0700

    Updated some code for review.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import SearchResults from '../SearchResults/SearchResults';
 import logo from './logo.svg';
 import './App.css';
 
